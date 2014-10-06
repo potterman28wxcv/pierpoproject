96169c6cd93707f5bd30bec64305a406c66df57b
Pierre Poupin <pierre.poupin@ensimag.grenoble-inp.fr>
2014-10-07 01:25:53 +0200

    Oublié de commit : best_fit à débugger

--- mem_alloc.c
+++ mem_alloc.c
@@ -3,6 +3,8 @@
 #include <assert.h>
 #include <string.h>
 
+#define BEST_FIT 
+
 /* #define DEBUG */
 
 /* memory */
@@ -26,6 +28,7 @@ free_block_t first_free;
 
 #define ULONG(x)((long unsigned int)(x))
 #define max(x,y) (x>y?x:y)
+#define min(x,y) (x<y?x:y)
 
 void memory_init(void){
 	free_block_s free_block = {MEMORY_SIZE, NULL};
@@ -35,8 +38,14 @@ void memory_init(void){
    	first_free = (free_block_t) memory;
 }
 
+
+
+
+/********************* MEMORY ALLOC **************************/
+
+/* FIRST FIT */
+#ifdef FIRST_FIT
 char *memory_alloc(int size){
-	/* First Fit */
 
 	free_block_t current_free;
 	free_block_t previous;
@@ -117,6 +126,87 @@ char *memory_alloc(int size){
 #endif
 	return ((char*) current_free) + sizeof(busy_block_s);
 }
+#endif
+
+
+
+/***** BEST FIT *****/
+#ifdef BEST_FIT
+char *memory_alloc(int size){
+
+	int min_remaining_size = -1;
+	free_block_t best_block = NULL;
+	free_block_t best_previous = NULL;
+
+	free_block_t current_free;
+	free_block_t previous;
+	free_block_t new_free;
+	free_block_s new_free_s;
+
+	current_free = first_free;
+	previous = first_free;
+
+	if (first_free == NULL) {
+		printf("first_free does not exist.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Browse through the free_block list */
+	while (current_free != NULL) {
+		printf("Go into first while\n");
+		previous = current_free;
+		if (current_free->size < size + sizeof(busy_block_s)) {
+			printf("Go into second if\n");
+			/* Not sure about the condition */
+			if (min_remaining_size == -1 || (size + sizeof(busy_block_s) - current_free->size < min_remaining_size)) {
+				printf("Go into last if\n");
+				best_block = current_free;
+				min_remaining_size = size + sizeof(busy_block_s) - current_free->size;
+				best_previous = previous;
+			}
+		}
+		current_free = current_free->next;
+	}
+	printf("Remaining size : %i\n",min_remaining_size);
+	
+
+	/* If we went through the whole list and didn't find a match, then the memory is full */
+	if (min_remaining_size == -1) {
+		printf("Not enough memory space.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* We now allocate the block */
+
+	/* New pointer to the beginning of the new free block */
+	new_free = (free_block_s*) (((char*)best_block) + size + sizeof(busy_block_s));
+	/* Write the new size left in the structure */
+	new_free_s.size = best_block->size - size - sizeof(busy_block_s);
+	/* Write the new next position in the structure */
+	new_free_s.next = best_block->next;
+	memcpy(new_free, &new_free_s, sizeof(free_block_s));
+
+	/* Now we have to replace the old free block by a busy one */
+	best_block->size = size + sizeof(busy_block_s);
+
+	/* previous -> new_free */
+	/* Works even if we are on first_free */
+	if (best_previous == first_free) {
+		first_free = new_free;
+	} else {
+		best_previous->next = new_free;
+	}
+
+ 	print_alloc_info((char*) best_block + sizeof(busy_block_s), best_block->size - sizeof(busy_block_s)); 
+
+	return ((char*) best_block) + sizeof(busy_block_s);
+}
+#endif
+
+
+
+
+/************************ MEMORY FREE **********************/
 
 /**
  * Merge the block to the left free neighbour, 
